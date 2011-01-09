63d5c0d1dc21e820dffd71b9f9a04e299fc63f6c
Yujian Zhang <yujian.zhang@gmail.com>
2011-01-09 16:04:39 +0800

    Add Piece and Position to BitBoard.scala and copyright information.

--- project/build/UniChess.scala
+++ project/build/UniChess.scala
@@ -6,6 +6,7 @@
  * License: 
  *   GNU General Public License v2
  *   http://www.gnu.org/licenses/gpl-2.0.html
+ * Copyright (C) 2010-2011 Yujian Zhang
  */
 
 import sbt._
@@ -15,5 +16,6 @@ class UniChess(info: ProjectInfo) extends DefaultProject(info) {
 
   val scalatest = "org.scalatest" % "scalatest" % "1.2"
   val specs = "org.specs" % "specs" % "1.4.3"
-  val scalacheck = "org.scala-tools.testing" % "scalacheck_2.8.0-SNAPSHOT" % "1.8-SNAPSHOT"
+  val scalacheck = "org.scala-tools.testing" % "scalacheck_2.8.0-SNAPSHOT" % 
+                    "1.8-SNAPSHOT"
 }
--- src/main/scala/net/whily/unichess/engine/BitBoard.scala
+++ src/main/scala/net/whily/unichess/engine/BitBoard.scala
@@ -6,10 +6,12 @@
  * License: 
  *   GNU General Public License v2
  *   http://www.gnu.org/licenses/gpl-2.0.html
+ * Copyright (C) 2010-2011 Yujian Zhang
  */
 
 package net.whily.unichess.engine
 
+/// **********************************************************************
 /// For details on definitions of square, files and ranks, please refer
 ///   http://chessprogramming.wikispaces.com/Square+Mapping+Considerations
 
@@ -26,6 +28,7 @@ object Square extends Enumeration {
       A8, B8, C8, D8, E8, F8, G8, H8 = Value
   implicit def squareToInt(x: Square): Int = x.id
 }
+import Square._
 
 // Little endian mapping of files.
 object File extends Enumeration {
@@ -33,6 +36,7 @@ object File extends Enumeration {
   val FileA, FileB, FileC, FileD, FileE, FileF, FileG, FileH = Value
   implicit def fileToInt(x: File): Int = x.id
 }
+import File._
 
 // Little endian mapping of ranks.
 object Rank extends Enumeration {
@@ -40,3 +44,58 @@ object Rank extends Enumeration {
   val Rank1, Rank2, Rank3, Rank4, Rank5, Rank6, Rank7, Rank8 = Value
   implicit def rankToInt(x: Rank): Int = x.id
 }
+import Rank._
+
+/// **********************************************************************
+/// Position definition.
+
+object Piece extends Enumeration {
+  type Piece = Value
+  val Occupied, Pawn, Knight, Bishop, Rook, Queen, King = Value
+  implicit def pieceToInt(x: Piece): Int = x.id
+}
+import Piece._
+
+object PieceV {
+  val EmptyV  = 0
+  val PawnV   = 1
+  val KnightV = 3
+  val BishopV = 3 
+  val RookV   = 5
+  val QueenV  = 9
+  val KingV   = 99 
+}
+import PieceV._
+
+object Color extends Enumeration {
+  type Color = Value
+  val Black, White = Value
+  implicit def colorToInt(x: Color) = x.id
+}
+import Color._
+
+object BitBoard {
+  type BitBoard = Long
+}
+import BitBoard._
+
+/* Bitboards for one side. */
+abstract class Pieces {
+  val pieces: Array[BitBoard]  // Size is 7
+}
+
+/** Defined according to FEN definition in
+ *  http://en.wikipedia.org/wiki/Forsyth-Edwads_Notation
+ */
+abstract class Position {
+  var allPieces: Array[Pieces]  // Size is 2
+  var activeColor: Color
+  var castlingAvailability: Int
+  var enPassantTarget: Int
+  var halfMoveCloce: Int
+  var fullMoveNumber: Int
+
+  def toFEN: String
+}
+
+
