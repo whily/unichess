99e64a7fd20f4bd6ebf5495a3b417407f256be00
Yujian Zhang <yujian.zhang@gmail.com>
2011-01-22 00:43:07 +0800

    Upgrade ScalaTest to 1.2.1-SNAPSHOT.

--- project/build/UniChess.scala
+++ project/build/UniChess.scala
@@ -14,7 +14,7 @@ import sbt._
 class UniChess(info: ProjectInfo) extends DefaultProject(info) {
   val scalaToolsSnapshots = ScalaToolsSnapshots
 
-  val scalatest = "org.scalatest" % "scalatest" % "1.2"
+  val scalatest = "org.scalatest" % "scalatest" % "1.2.1-SNAPSHOT"
   val specs = "org.specs" % "specs" % "1.4.3"
   val scalacheck = "org.scala-tools.testing" % "scalacheck_2.8.0-SNAPSHOT" % 
                     "1.8-SNAPSHOT"
