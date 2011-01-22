56cfbc7fa3a8aa5dc684b027204cd54f7801a9b3
Yujian Zhang <yujian.zhang@gmail.com>
2011-01-22 13:28:45 +0800

    Add simple popcount related functions.

--- src/main/scala/net/whily/unichess/engine/PopCount.scala
+++ src/main/scala/net/whily/unichess/engine/PopCount.scala
@@ -2,7 +2,8 @@
  * Author: 
  *   Yujian Zhang <yujian{dot}zhang[at]gmail(dot)com>
  * Description:
- *   Count the number of 1's in BitBoard.
+ *   Population count. See
+ *     http://chessprogramming.wikispaces.com/Population+Count
  * License: 
  *   GNU General Public License v2
  *   http://www.gnu.org/licenses/gpl-2.0.html
@@ -12,11 +13,22 @@
 package net.whily.unichess.engine
 
 object PopCount {
-  /**
-   * Count the number of 1's in BitBoard.
-   *
-   * @see SWAR algorithm http://chessprogramming.wikispaces.com/Population+Count
-   */
+  // Return true if BB is empty.
+  def emptyBB(x: Long): Boolean = x == 0L
+
+  // Return true if BB is not empty.
+  def notEmptyBB(x: Long): Boolean = x != 0L
+
+  // Return true if population count of BB is 1.
+  def singleBB(x: Long): Boolean = (x != 0L) && ((x & (x - 1L)) == 0L)
+
+  // Return true if population count <= 1.
+  def emptyOrSingleBB(x: Long): Boolean = (x & (x - 1L)) == 0L
+
+  // Return true if population count > 1.
+  def twoOrMoreBB(x: Long): Boolean = (x & (x - 1L)) != 0L
+  
+  // Count the number of 1's in BitBoard. Use SWAR algorithm.
   def popCount(x: Long): Long = {
     val k1 = 0x5555555555555555L
     val k2 = 0x3333333333333333L
---
+++ src/test/scala/net/whily/unichess/engine/PopCountCheck.scala
@@ -0,0 +1,36 @@
+/**
+ * Author: 
+ *   Yujian Zhang <yujian{dot}zhang[at]gmail(dot)com>
+ * Description:
+ *   Check for popCount.
+ * License: 
+ *   GNU General Public License v2
+ *   http://www.gnu.org/licenses/gpl-2.0.html
+ * Copyright (C) 2010-2011 Yujian Zhang
+ */
+
+import org.scalatest.junit.JUnit3Suite
+import org.scalatest.prop.Checkers
+import org.scalacheck.Prop._
+import org.scalacheck.Arbitrary._
+
+import net.whily.unichess.engine.PopCount._
+
+class PopCountSuite extends JUnit3Suite with Checkers {
+  def testPopCount() {
+    def naivePopCount(x: Long): Long = {
+      var count = 0L
+      var y = x
+      for (i <- 0 until 64) {
+        count = count + (y & 1L)
+        y = y >> 1
+      }
+      count
+    }
+
+    assert(popCount(-1L) === 64)
+    check((x: Long) => popCount(x) == naivePopCount(x))
+  }
+}
+
+
